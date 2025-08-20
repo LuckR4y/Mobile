79a62c39ce07c265ce6d1e9237724bc314ebc7d1
LuckR4y <137362549+LuckR4y@users.noreply.github.com>
2025-08-20 20:19:57 -0300

    Update ExerciciosPart1.kt

--- ExerciciosPart1.kt
+++ ExerciciosPart1.kt
@@ -14,17 +14,46 @@ fun main() {
     }
 }
 
-//1) Variáveis e Tipos básicos
+/* 
+1) Variáveis e Tipos básicos
+Declare variáveis para armazenar nome (String), idade (Int) e altura (Double).
+ Implemente a função informacoes() que retorne uma string formatada no estilo:
+ "Nome: X, Idade: Y, Altura: Z m"
+
+fun main() {
+    println(informacoes("João", 30, 1.75))
+    // Saída esperada: Nome: João, Idade: 30, Altura: 1.75 m
+}
+*/
 fun informacoes(nome: String, idade: Int, altura: Double) = "Nome: $nome, Idade: $idade, Altura: ${"%.2f".format(altura)} m"
 
 
-//2) Coleções e Loops (ranges)
+/*
+2) Coleções e Loops (ranges)
+Implemente a função paresAteN(n: Int): List<Int> que retorne uma lista com todos os números pares de 0 até n (inclusive).
+fun main() {
+    println(paresAteN(10))
+    // Saída esperada: [0, 2, 4, 6, 8, 10]
+}
+*/
 fun paresAteN(n: Int): List<Int> {
     return (0..n).filter { it % 2 == 0 }
 }
 
 
-//3) Fluxo de controle
+/*
+3) Fluxo de controle
+Implemente a função classificarNota(nota: Double): String que retorne:
+"Aprovado" se a nota for >= 6.0
+"Recuperação" se estiver entre 4.0 e 5.9
+"Reprovado" caso contrário.
+
+fun main() {
+    println(classificarNota(7.5)) // Aprovado
+    println(classificarNota(5.0)) // Recuperação
+    println(classificarNota(3.0)) // Reprovado
+}
+*/
 fun classificarNota(nota: Double): String {
     return when {
         nota >= 6.0 -> "Aprovado"
@@ -33,7 +62,14 @@ fun classificarNota(nota: Double): String {
     }
 }
 
-//4) Funções
+/*
+4) Funções
+Implemente a função fatorial(n: Int): Long que calcule o fatorial de n usando loop.
+
+fun main() {
+    println(fatorial(5)) // 120
+}
+*/
 fun fatorial(n: Int): Long {
     var resultado = 1L
     for (i in 1..n){
@@ -42,7 +78,15 @@ fun fatorial(n: Int): Long {
     return resultado
 }
 
-//5) Segurança nula
+/*
+5) Segurança nula
+Implemente a função tamanhoSeguro(texto: String?): Int que retorne o tamanho da string se ela não for nula ou 0 caso seja nula.
+
+fun main() {
+    println(tamanhoSeguro("Kotlin")) // 6
+    println(tamanhoSeguro(null))     // 0
+}
+*/
 fun tamanhoSeguro(texto: String?): Int{
     return texto?.length ?:0
 }
